1d22505869abf8321db1b2d59fca273262df79ab
Jakaria030 <gulamjakaria99@gmail.com>
2025-11-16 18:41:33 +0600

    fix issues

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,13 @@ import config from "./config/env";
 
 connectDB()
   .then(() => {
-    const PORT = config.port || 5000;
+    // const PORT = config.port || 5000;
 
-    app.listen(PORT, () => {
-      console.log(`Server is running on port: ${PORT}`);
-    });
-
+    // app.listen(PORT, () => {
+    //   console.log(`Server is running on port: ${PORT}`);
+    // });
+    
+   console.log(`Server is running`);
   }).catch((error) => {
     console.log("MongoDB connection failed !! ", error);
   });
